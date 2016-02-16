5cf169e19d225c69efd9b7f6633762c4cdc1d713
kyon <kyon0304@gmail.com>
2016-02-16 18:14:36 +0800

    worked, save preview as pdf

--- src/renderer/components/main.jsx
+++ src/renderer/components/main.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import shell from 'shell';
 import Editor from './MDEditor.js'
 import marked from 'marked'
+import jsPDF from '../../../bower_components/jspdf'
 
 export class Main extends React.Component {
   state = {
@@ -24,14 +25,24 @@ export class Main extends React.Component {
   openGithub () {
     shell.openExternal('https://github.com/Quramy/electron-jsx-babel-boilerplate');
   }
+
   render() {
     var preview = marked(this.state.code)
+      , pdf = new jsPDF('p', 'pt', 'letter')
+      , margins = { top: 80, bottom: 60, left: 40, width: 522 }
+
+    var savePDF = function () {
+      pdf.fromHTML(preview, margins.left, margins.top, { 'width': margins.width },
+                   function() { pdf.save("test.pdf") }, margins)
+                   //() => pdf.save("test.pdf"), margins)
+    }
 
     return (
       <div className="container">
         <div className="content">
           <div className="editor">
             <Editor value={ this.state.code } onChange={ this.updateCode } />
+            <button className="save" onClick={ savePDF }>Download PDF</button>
           </div>
           <div className="preview" dangerouslySetInnerHTML={{ __html: preview }} />
         </div>
